d0306c94e47c3a1d42fd46e25b411d200bbcd89e
Lima001 <limaedugabriel@gmail.com>
2023-09-26 16:55:50 -0300

    Ajuste fila multicast

--- P2P/client.py
+++ P2P/client.py
@@ -8,6 +8,19 @@ ADDR = '224.3.29.71'
 PORT = 10000
 CLIENT_DIR = "client_files/"
 
+def clear_buffer(sock):
+    while True:
+        try:
+            data = sock.recv(1024)
+            if not data:
+                break
+            else:
+                print("liberando socket....")
+        except:
+            print("socket vazio!")
+            break
+
+
 class Client:
 
     def __init__(self, addr_group, port, client_dir):
@@ -46,6 +59,8 @@ class Client:
                 while True:
                     #print (sys.stderr, 'waiting to receive')
                     print('--> waiting to receive')
+                    # limpa o socket
+                    #clear_buffer(sock)
                     try:
                         data, server = sock.recvfrom(1024)
 
@@ -56,11 +71,25 @@ class Client:
                     else:
                         parsed_data = json.loads(data.decode()) 
                         #print (sys.stderr, 'received "%s" from %s' % (data, server))
-                        print('--> received from %s', server)
+                        print(f'--> received from {server}: {parsed_data}')
                         
                         with open(os.path.join(self.client_dir, message), 'w') as file:
                             file.write(parsed_data['detail'])
                         
+                        # Recebeu, mas tenta receber respostas de outros nós que possuem o arquivo
+                        while True:
+                            #print (sys.stderr, 'waiting to receive')
+                            print('*** jogando fora')
+                            # limpa o socket
+                            #clear_buffer(sock)
+                            try:
+                                data, server = sock.recvfrom(1024)
+
+                            except socket.timeout:
+                                #print (sys.stderr, 'timed out, no more responses')
+                                print('*** timed out, no more responses')
+                                break
+
                         break
             finally:
                 pass
